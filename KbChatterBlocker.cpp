dc62b8aad51fbe09c6d02453a23ef06b17a68a0a
Slamb <slamb@slamb.eu>
2026-01-04 02:37:31 +0100

    Create KbChatterBlocker.cpp

---
+++ KbChatterBlocker.cpp
@@ -0,0 +1,53 @@
+#include <windows.h>
+#include <unordered_map>
+
+constexpr DWORD CHATTER_THRESHOLD_MS = 100;
+
+HHOOK g_hook = nullptr;
+std::unordered_map<DWORD, DWORD> lastPressTime;
+
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+    if (nCode == HC_ACTION && wParam == WM_KEYDOWN)
+    {
+        KBDLLHOOKSTRUCT* kb = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
+        DWORD key = kb->vkCode;
+        DWORD now = GetTickCount();
+
+        auto it = lastPressTime.find(key);
+        if (it != lastPressTime.end())
+        {
+            if (now - it->second < CHATTER_THRESHOLD_MS)
+            {
+                return 1; // blokuj chatter
+            }
+        }
+
+        lastPressTime[key] = now;
+    }
+
+    return CallNextHookEx(g_hook, nCode, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int)
+{
+    g_hook = SetWindowsHookEx(
+        WH_KEYBOARD_LL,
+        KeyboardProc,
+        hInst,
+        0
+    );
+
+    if (!g_hook)
+        return 1;
+
+    MSG msg;
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    UnhookWindowsHookEx(g_hook);
+    return 0;
+}
